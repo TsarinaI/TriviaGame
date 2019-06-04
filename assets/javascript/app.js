a5baa84fd835e5a7853d93de25eec6b33565b0a7
Tsarina <tsarina.islam@gmail.com>
2019-06-04 11:29:39 -0400

    beginning, with questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,50 @@
+var questions = [{
+    question: "What device helps trainers identify Pokemon?",
+    answers: ["Polyjuice Potion", "Hyper Potion", "Bulbasaur", "Pokedex"],
+    correctAnswer: "Pokedex",
+}, {
+    question: "Which gaming console did the first six Pokemon games come out on?",
+    answers: ["Gameboy Color", "Nintendo DS", "Gameboy", "PlayStation"],
+    correctAnswer: "Gameboy"
+}, {
+    question: "Which Pokemon is #039?",
+    answers: ["Pikachu", "Jigglypuff", "Snorlax", "Ninetails"],
+    correctAnswer: "Jigglypuff"
+}, {
+    question: "What Gameboy game/s was/were released in February, 1996?",
+    answers: ["Pokemon Yellow (Japan)", "Pokemon Red and Green (Japan)", "Pokemon Blue (Japan)", "Pokemon Red and Blue (North America)"],
+    correctAnswer: "Pokemon Red and Green (Japan)"
+}, {
+    question: "Gloom evolves into which Pokemon?",
+    answers: ["Vileplume", "Chikorita", "Celebi", "Oddish"],
+    correctAnswer: "Vileplume"
+}, {
+    question: "What number Pokemon is Arcanine?",
+    answers: ["#013", "#069", "#059", "#159"],
+    correctAnswer: "#059"
+}, {
+    question: "Which type of Pokemon are Psychic Pokemon NOT weak to?",
+    answers: ["Bug", "Ghost", "Grass", "Dark"],
+    correctAnswer: "Grass"
+}, {
+    question: "Where do you find Articuno in Pokemon Red?",
+    answers: ["Fuschia City", "Seafoam Islands", "Cerulean City", "Mt. Moon"],
+    correctAnswer: "Seafoam Islands"
+}, {
+    question: "Which Pokemon game was the first to use AR (augmented reality) and also be released on iOS and Android?",
+    answers: ["Pokemon Sword and Shield", "Pokemon Let's Go Pikachu!", "Pokemon Quest", "Pokemon GO"],
+    correctAnswer: "Pokemon GO"
+}, {
+    question: "In the original TV series, why does Misty first begin following Ash?",
+    answers: ["She wanted his Pikachu", "She was in love with him", "She thought he was a skilled trainer", "He stole and wrecked her bike"],
+    correctAnswer: "He stole and wrecked her bike"
+}]
+
+
+$("#start").on("click", function() {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+});
+
