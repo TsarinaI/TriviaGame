e049ccae54346ba8a5af2cda77632ce2ba5bb316
Tsarina <tsarina.islam@gmail.com>
2019-06-05 12:18:00 -0400

    Tried to insert questions in append for loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -42,8 +42,10 @@ var questions = [{
 
 
 $("#start").on("click", function() {
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
+    for (let i = 0; i < questions.length; i++) {
+        $("#subwrapper").append("<h2>"+questions[i]).question+"</h2>";
+        for (var j=0; j,questions[i].answers.length; j++);
+        $("#subwrapper").append("<input type='radio' name= 'question-'" +i +"' value='"+ questions[i].answers[j]+"'>'"+questions[i].answers[j]);
         
     }
 });
