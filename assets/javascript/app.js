88bbd3e9f59645f38ca9bc77d05b197eb5187bb7
Tsarina <tsarina.islam@gmail.com>
2019-09-20 23:13:41 -0400

    is there anything to commit?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+var card = $("#quizSpace");
+
 var questions = [{
     question: "What device helps trainers identify Pokemon?",
     answers: ["Polyjuice Potion", "Hyper Potion", "Bulbasaur", "Pokedex"],
@@ -38,15 +40,67 @@ var questions = [{
     question: "In the original TV series, why does Misty first begin following Ash?",
     answers: ["She wanted his Pikachu", "She was in love with him", "She thought he was a skilled trainer", "He stole and wrecked her bike"],
     correctAnswer: "He stole and wrecked her bike"
-}]
+}];
+
+var timer;
+
+var trivGame = {
+    correct: 0,
+    incorrect: 0,
+    counter: 60,
+
+    countdown: function () {
+        game.counter--;
+        $("#counter-number").html(trivGame.counter);
+        if (trivGame.counter === 0) {
+            console.log("TIME UP");
+            game.done();
+        }
+    },
+
+    start = function () {
+        timer = setInterval(trivGame.countdown, 1000);
+
+        $("#sub-wrapper").prepend(
+            "<h2>Time Remaining: <span id='counter-number'>60</span> Seconds</h2>");
+
+        $("#start").remove();
+
+        for (let i = 0; i < questions.length; i++) {
+            card.append("<h2>" + questions[i]).question + "</h2>";
+            for (var j = 0; j, questions[i].answers.length; j++) {
+                card.append("<input type='radio' name= 'question-'" + i + "' value='" + questions[i].answers[j] + "'>'" + questions[i].answers[j])
+            };
+        }
+
+        card.append("<button id='done'>Done</button>");
+    },
+
+    done: function() {
+        var inputs = card.children("input:checked");
+        for (var i = 0; i < inputs.length; i++) {
+          if ($(inputs[i]).val() === questions[i].correctAnswer) {
+            trivGame.correct++;
+          } else {
+            trivGame.incorrect++;
+          }
+        }
+        this.result();
+      },
+    
+      result: function() {
+        clearInterval(timer);
+    
+        $("#sub-wrapper h2").remove();
+    
+        card.html("<h2>All Done!</h2>");
+        card.append("<h3>Correct Answers: " + this.correct + "</h3>");
+        card.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+      }
+
+};
+
+$("#start").on("click", function () {
 
 
-$("#start").on("click", function() {
-    for (let i = 0; i < questions.length; i++) {
-        $("#subwrapper").append("<h2>"+questions[i]).question+"</h2>";
-        for (var j=0; j,questions[i].answers.length; j++);
-        $("#subwrapper").append("<input type='radio' name= 'question-'" +i +"' value='"+ questions[i].answers[j]+"'>'"+questions[i].answers[j]);
-        
-    }
 });
-
